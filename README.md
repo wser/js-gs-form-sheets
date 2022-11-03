9852235d48334d5f2105e7f13b65ea003958abc7
wser <w.web.dev@gmail.com>
2022-11-03 12:35:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-gs-form-sheets
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-mslvqs)
---
+++ index.js
@@ -0,0 +1,17 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const scriptURL =
+  'https://script.google.com/macros/s/AKfycbzkIeAzPnqCkJXkGi4hSZQ9rDGweR8mgMA3oqQiD40jivRmt5pAC7WSP2Htn4PmKa_1/exec';
+const form = document.forms['submit-to-google-sheet'];
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => console.log('Success!', response))
+    .catch((error) => console.error('Error!', error.message));
+});

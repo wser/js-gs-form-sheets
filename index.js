a9eacb7943895aa01a0d477270aad3506346cb9d
wser <w.web.dev@gmail.com>
2022-11-03 14:30:24 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 // Import stylesheets
 import './style.css';
 
+// google script path
+// tutorial: https://github.com/MusabDev/save-html-form-to-google-sheets
+const scriptURL =
+  'https://script.google.com/macros/s/AKfycbzkIeAzPnqCkJXkGi4hSZQ9rDGweR8mgMA3oqQiD40jivRmt5pAC7WSP2Htn4PmKa_1/exec';
+
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const $ = (e) => document.querySelector(e);
+
+const appDiv = $('#app');
+const form = $('#form');
+
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 fetch('https://hutils.loxal.net/whois')
-  // Extract JSON body content from HTTP response
-  .then((response) => response.json())
+  .then((response) => response.json()) // Extract JSON body content from HTTP response
   // Do something with the JSON data
   .then((data) => {
-    console.log(JSON.stringify(data, null, 2));
+    //let formFill = Object.entries(data).map(([key, value]) => value);
 
-    var result = JSON.stringify(data, null, 2);
-    appDiv.innerHTML += result;
+    Object.entries(data).forEach(
+      ([key, value]) => ($(`#${key}`) = value)
+    );
+  })
+  .then(() => {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then((response) => console.log('Success!', response))
+        .then(() => form.reset())
+        .catch((error) => console.error('Error!', error.message));
+    });
   });

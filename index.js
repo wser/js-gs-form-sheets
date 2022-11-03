fbb33b98ac97cbb720816b3a09abb04ffb5cef68
wser <w.web.dev@gmail.com>
2022-11-03 13:21:26 +0100

    1 file modified

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
+fetch('https://hutils.loxal.net/whois')
+  // Extract JSON body content from HTTP response
+  .then((response) => response.json())
+  // Do something with the JSON data
+  .then((data) => {
+    console.log(JSON.stringify(data, null, 2));
+
+    var result = JSON.stringify(data, null, 2);
+    appDiv.innerHTML += result;
+  });

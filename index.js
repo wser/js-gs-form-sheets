ea2d9376e07cf2785c800d2863926e0529c6e697
wser <w.web.dev@gmail.com>
2022-11-04 08:16:55 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Import stylesheets
 import './style.css';
 
-// google script path
+// google script path for web app script
 // tutorial: https://github.com/MusabDev/save-html-form-to-google-sheets
 const scriptURL =
   'https://script.google.com/macros/s/AKfycbzkIeAzPnqCkJXkGi4hSZQ9rDGweR8mgMA3oqQiD40jivRmt5pAC7WSP2Htn4PmKa_1/exec';
+
+const ipCheck = 'https://hutils.loxal.net/whois';
 
 // Write Javascript code!
 const $ = (e) => document.querySelector(e);
@@ -13,33 +15,32 @@ const appDiv = $('#app');
 const form = $('#form');
 const btn = $(`[name=submit]`);
 
+const makeInputField = (key, value) => {
+  let input = document.createElement('input');
+  input.name = key;
+  input.type = 'text';
+  input.placeholder = key;
+  input.value = value;
+  input.style.marginRight = '4px';
+  //input.style.display = "none"
+  return input;
+};
+
 appDiv.innerHTML = `<h1>JS Starter</h1><hr><br/>`;
 /* prettier-ignore */
-fetch('https://hutils.loxal.net/whois')
+fetch(ipCheck)
   .then((response) => response.json()) // Extract JSON body content from HTTP response
   // Do something with the JSON data
   .then((data) => {
     Object.entries(data).forEach(([key, value]) => {
-      try { 
-        let input = document.createElement("input");
-        input.name = key;
-        input.type = "text";
-        input.placeholder = key;
-        input.value = value
-        input.style.marginRight = "4px"
-        input.style.display = "none"
-
-        form.insertBefore(input, btn)
-
-        //form.appendChild(input))
-        //$(`[name='${key}']`).value = value;
-      } 
+      try { form.insertBefore(makeInputField(key, value), btn) } 
       catch (err) {console.log('GREŠKA:', err); }
     });
     // let obj = JSON.stringify(data, null, 2);
     // console.log(obj);
   });
 
+// post data to google sheets
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })

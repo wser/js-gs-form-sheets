7c21b515c10044952d0c3f4de8f65f94b649e8d6
wser <w.web.dev@gmail.com>
2022-11-04 13:15:02 +0100

    2 files modified

--- index.js
+++ index.js
@@ -85,12 +85,4 @@ const createApp = () => {
   appDiv.append(form); // add the generated form to div
 };
 
-if (document.readyState !== 'loading') {
-  console.log('document is already ready, just execute code here');
-  createApp();
-} else {
-  document.addEventListener('DOMContentLoaded', function () {
-    console.log('document was not ready, place code here');
-    createApp();
-  });
-}
+createApp();
